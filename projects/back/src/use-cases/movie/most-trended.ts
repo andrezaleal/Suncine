3b9cec6d45e933332c42f380a4d597f6269b1188
Henrique Artur <contato@henriqueartur.com>
2024-11-16 22:01:03 -0300

    ♻️ use MovieInApp in Most-trended

--- projects/back/src/use-cases/movie/most-trended.ts
+++ projects/back/src/use-cases/movie/most-trended.ts
@@ -1,8 +1,18 @@
-import { get10MostPopular } from "../../domain/movie/calcs"
+import { formatBackdropImg, formatPosterImg, get10MostPopular } from "../../domain/movie/calcs"
+import type { MovieInApp } from "../../domain/movie/type"
 import type { TmdbApiContract } from "../../infra/tmdb"
 
-export async function mostTrendedUseCase(MOVIE_API: TmdbApiContract) {
+export async function mostTrendedUseCase(MOVIE_API: TmdbApiContract): Promise<MovieInApp> {
   const tmd_br_trend_page_1 = await MOVIE_API.trendingMovies()
   const [most_trended] = get10MostPopular(tmd_br_trend_page_1)
-  return most_trended
+  return {
+    user_liked: false,
+    backdrop_path: formatBackdropImg(most_trended.backdrop_path),
+    poster_path: formatPosterImg(most_trended.poster_path),
+    title: most_trended.title,
+    original_title: most_trended.original_title,
+    tmdb_id: most_trended.id,
+    id: "",
+    likes: 0
+  }
 }
