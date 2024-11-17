a959b82d97d959faa7df7ab43abc1d8012d08df8
Henrique Artur <contato@henriqueartur.com>
2024-11-16 22:57:38 -0300

    ✨ start app

--- projects/back/src/index.ts
+++ projects/back/src/index.ts
@@ -1,5 +1,9 @@
 import Fastify from "fastify"
 import { addRouters } from "./infra/routers"
+import mongoose from "mongoose"
+import { ENV_VAR } from "../env"
+import { RunSeed } from "./infra/seed"
+import { DB } from "./infra/db"
 
 const fastify = Fastify({
   logger: true
@@ -13,10 +17,21 @@ fastify.get("/", () => ({
 
 addRouters(fastify)
 
-fastify
-  .listen({ port: PORT })
-  .then(() => console.info(`Server running on port: ${PORT}`))
-  .catch((err) => {
-    fastify.log.error(err)
+async function Start() {
+  try {
+    await mongoose.connect(
+      `mongodb://${ENV_VAR.MONGO.LOGIN}:${ENV_VAR.MONGO.PASS}@${ENV_VAR.MONGO.HOST}:${ENV_VAR.MONGO.PORT}`
+    )
+    console.info("Mongo connected!")
+    await RunSeed(DB(), ENV_VAR)
+    await fastify.ready()
+    await fastify.listen({ port: PORT })
+    console.info(`Server running on port: ${PORT}`)
+  } catch (error) {
+    console.error(error)
+    fastify.log.error(error)
     process.exit(1)
-  })
+  }
+}
+
+Start()
