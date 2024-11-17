e54f265a524b8e0b32b17766fc08f00c3dc963bb
Henrique Artur <contato@henriqueartur.com>
2024-11-17 15:58:44 -0300

    ✨ find one like

--- projects/back/src/infra/db/op/movies/index.ts
+++ projects/back/src/infra/db/op/movies/index.ts
@@ -1,27 +1,31 @@
 import type { DbModels } from "../.."
 import type { TmdbMovie } from "../../../tmdb/type"
+import { findLike } from "./find-like"
 import { findUserLikes } from "./find-likes"
 import { findOrCreateByTmdbIds } from "./find-or-create-by-tmdb-ids"
 import { findUserLikesWithMovieId } from "./find-user-likes"
 import { like } from "./like"
 import { unlike } from "./unlike"
 
-export function MoviesOp(MODEL: DbModels) {
+export function MoviesOp(MODELS: DbModels) {
   return {
     findOrCreateByTmdbIds(tmdb_movies: TmdbMovie[]) {
-      return findOrCreateByTmdbIds(MODEL, tmdb_movies)
+      return findOrCreateByTmdbIds(MODELS, tmdb_movies)
     },
     findUserLikes(user_id: string) {
-      return findUserLikes(MODEL, user_id)
+      return findUserLikes(MODELS, user_id)
     },
     findUserLikesWithMovieId(user_id: string, tmdb_ids: number[]) {
-      return findUserLikesWithMovieId(MODEL, user_id, tmdb_ids)
+      return findUserLikesWithMovieId(MODELS, user_id, tmdb_ids)
     },
     like(user_id: string, tmdb_id: number) {
-      return like(MODEL, user_id, tmdb_id)
+      return like(MODELS, user_id, tmdb_id)
     },
     unlike(user_id: string, tmdb_id: number) {
-      return unlike(MODEL, user_id, tmdb_id)
+      return unlike(MODELS, user_id, tmdb_id)
+    },
+    findLike(user_id: string, tmdb_id: number) {
+      return findLike(MODELS, user_id, tmdb_id)
     }
   }
 }
